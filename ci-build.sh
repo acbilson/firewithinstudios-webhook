e2ecb02f0b28f3fd714f8f45a4e1b801d15581de
Alex Bilson <acbilson@gmail.com>
2022-08-15 13:57:16 -0600

    removes caching

--- ci-build.sh
+++ ci-build.sh
@@ -4,7 +4,7 @@ selected_rev=${1:0:7}
 current_rev=$(git rev-parse --short HEAD)
 
 if [ $selected_rev = $current_rev ]; then
-   podman build --target=prod -t acbilson/firewithin-webhook-$current_rev:alpine -t acbilson/firewithin-webhook:latest .;
+   podman build --target=prod --no-cache -t acbilson/firewithin-webhook-$current_rev:alpine -t acbilson/firewithin-webhook:latest .;
 else
    echo "the current git commit and the selected commit do not match";
 fi
